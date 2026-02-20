fd4fe8b66486b5b46702528a302c1d68fdf23096
artem <v.amuzinsky@yandex.ru>
2026-02-20 09:23:48 +0300

    lab1: переделал тесты в 1 части

--- src/main/java/org/example/Task1.java
+++ src/main/java/org/example/Task1.java
@@ -1,17 +1,19 @@
 package org.example;
 
 public class Task1 {
-    public static double sin(double x){
+    public static double sin(double x) {
         double sumNew, sumOld, sum;
         int i = 1;
         sum = sumNew = x;
         do {
             sumOld = sumNew;
-            i++; sum = sum * x * x / i;
-            i++; sum = sum / i;
+            i++;
+            sum = sum * x * x / i;
+            i++;
+            sum = sum / i;
             sum = -sum;
             sumNew = sumOld + sum;
-        }while( sumNew != sumOld);
+        } while (sumNew != sumOld);
         System.out.println(sumNew + " <- Teylor | Java sin -> " + Math.sin(x));
         return sumNew;
     }
--- src/test/java/Task1Test.java
+++ src/test/java/Task1Test.java
@@ -1,42 +1,32 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.example.Task1;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class Task1Test {
 
-    private static final double EPS = 1e-6;
-
-    @Test
-    void testZero() {
-        assertEquals(0.0, Task1.sin(0), EPS);
+    @ParameterizedTest
+    @CsvFileSource(resources = "/reference_values.csv", numLinesToSkip = 1)
+    void testReferenceValues(double x, double expected) {
+        assertEquals(expected, Task1.sin(x), Math.E);
     }
 
-    @Test
-    void testSmallValues() {
-        double x = 0.001;
-        assertEquals(Math.sin(x), Task1.sin(x), EPS);
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dense_range.csv", numLinesToSkip = 1)
+    void testDenseRange(double x, double expected) {
+        assertEquals(expected, Task1.sin(x), 1e-7);
     }
 
-    @Test
-    void testTypicalValues() {
-        double[] values = {Math.PI / 6, Math.PI / 4, Math.PI / 2, Math.PI};
-
-        for (double x : values) {
-            assertEquals(Math.sin(x), Task1.sin(x), 1e-9);
-        }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/odd_values.csv", numLinesToSkip = 1)
+    void testOddProperty(double x) {
+        assertEquals(-Task1.sin(x), Task1.sin(-x), Math.E);
     }
 
-    @Test
-    void testNegativeValues() {
-        double x = 1.3;
-        assertEquals(-Task1.sin(x), Task1.sin(-x), EPS);
+    @ParameterizedTest
+    @CsvFileSource(resources = "/periodic_values.csv", numLinesToSkip = 1)
+    void testPeriodicity(double x) {
+        assertEquals(Task1.sin(x), Task1.sin(x + Math.PI*2), Math.E);
     }
-
-    @Test
-    void testLargeValues() {
-        double x = 20;
-        assertEquals(Math.sin(x), Task1.sin(x), EPS);
-    }
-
 }
